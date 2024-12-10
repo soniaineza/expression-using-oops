12eb733278b92fa9d3181f97096648254034c9c5
soniaineza <ntwariprince013@gmail.com>
2024-12-10 10:16:58 +0200

    expre

--- expre.py
+++ expre.py
@@ -1,21 +1,9 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def __init__(self, expression):
+        self.expression = expression
 
-    def subtract(self, a, b):
-        return a - b
+    def solve(self):
+        return eval(self.expression)
 
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b != 0:
-            return a / b
-        else:
-            return "Cannot divide by zero"
-
-calc = Calculator()
-print("Addition:", calc.add(1,2))
-print("Subtraction:", calc.subtract(5,2))
-print("Multiplication:", calc.multiply(1,3))
-print("Division:", calc.divide(9,3))
+exp = Calculator("5 + 3 * 2 / 4")
+print("Result:", exp.solve())
